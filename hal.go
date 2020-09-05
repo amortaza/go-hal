289964072454d1dd51bb23354683390bd284f50b
amortaza <amortaza@gmail.com>
2020-09-04 23:47:34 -0700

    Cleanup main HAL interface

--- hal.go
+++ hal.go
@@ -1,38 +1,5 @@
 package hal
 
-type ButtonAction int
-
-const (
-	Button_Action_Down ButtonAction = 1 + iota
-	Button_Action_Up
-)
-
-type Canvas interface {
-
-	Clear(red, green, blue float32)
-
-	GetWidth() int
-	GetHeight() int
-
-	Begin()
-	End()
-
-	Paint(seeThru bool, left, top int, alphas []float32)
-
-	Free()
-}
-
-type Graphics interface {
-
-	Clear(red, green, blue float32)
-
-	PushView(width, height int)
-
-	PopView()
-
-	NewCanvas(width, height int) Canvas
-}
-
 type HAL interface {
 
 	Start(
@@ -46,14 +13,14 @@ type HAL interface {
 
 		onWindowResize,
 
-		onMouseMove func(int,int),
+		onMouseMove func(x, y int),
 		onMouseButton func(MouseButton, ButtonAction),
 
 		onKey func(KeyboardKey, ButtonAction, bool, bool, bool))
 
 	GetWindowDim()(width, height int)
 
-	GetMousePos()(x,y int)
+	GetMousePos()(x, y int)
 
 	SetMouseCursor(cursor MouseCursor)
 
